2816330cf34b89f554bb567549df72333098af6c
tarlepp <tarmo.leppanen@protacon.com>
2017-01-20 15:19:09 +0200

    Added generic REST traits for anonymous users.

---
+++ src/App/Traits/Rest/Roles/Public/Count.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+/**
+ * /src/App/Traits/Rest/Roles/User/Count.php
+ *
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+namespace App\Traits\Rest\Roles\User;
+
+use App\Traits\Rest\Methods\Count as CountMethod;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Trait to add 'Count' action for REST resources for 'anonymous' users.
+ *
+ * @see \App\Traits\Rest\Methods\Count for detailed documents.
+ *
+ * @package App\Traits\Rest\Roles\User
+ * @author  TLe, Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+trait Count
+{
+    use CountMethod;
+
+    /**
+     * Count action for current resource.
+     *
+     * @Route("/count")
+     * @Route("/count/")
+     *
+     * @Method({"GET"})
+     *
+     * @throws  \UnexpectedValueException
+     * @throws  \LogicException
+     * @throws  \InvalidArgumentException
+     * @throws  NonUniqueResultException
+     * @throws  NoResultException
+     * @throws  HttpException
+     * @throws  MethodNotAllowedHttpException
+     *
+     * @param   Request     $request
+     *
+     * @return  Response
+     */
+    public function count(Request $request): Response
+    {
+        return $this->countMethod($request);
+    }
+}
