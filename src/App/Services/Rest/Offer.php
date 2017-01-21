5fadc8e125c54123670691be3dcbe921e8d21786
tarlepp <tarmo.leppanen@protacon.com>
2017-01-21 19:40:06 +0200

    Implemented Offer entity Workshop relation handling.

--- src/App/Services/Rest/Offer.php
+++ src/App/Services/Rest/Offer.php
@@ -7,11 +7,15 @@ declare(strict_types=1);
  */
 namespace App\Services\Rest;
 
+use App\Entity\Interfaces\EntityInterface;
 use App\Entity\Offer as Entity;
+use App\Entity\Workshop as WorkshopEntity;
 use App\Repository\Offer as Repository;
+use App\Services\Rest\Workshop as WorkshopService;
 use Doctrine\Common\Persistence\Proxy;
 
 // Note that these are just for the class PHPDoc block
+/** @noinspection UnknownInspectionInspection */
 /** @noinspection PhpHierarchyChecksInspection */
 /** @noinspection PhpSignatureMismatchDuringInheritanceInspection */
 
@@ -33,5 +37,62 @@ use Doctrine\Common\Persistence\Proxy;
  */
 class Offer extends Base
 {
-    // Implement custom service methods here
+    /**
+     * @var WorkshopService
+     */
+    private $workshopService;
+
+    /**
+     * Setter for Workshop service
+     *
+     * @param   WorkshopService $workshopService
+     *
+     * @return  Offer
+     */
+    public function setWorkshopService(WorkshopService $workshopService)
+    {
+        $this->workshopService = $workshopService;
+
+        return $this;
+    }
+
+    /**
+     * Before lifecycle method for create method.
+     *
+     * @param   \stdClass               $data
+     * @param   EntityInterface|Entity  $entity
+     */
+    public function beforeCreate(\stdClass $data, EntityInterface $entity)
+    {
+        $this->processFormData($data);
+    }
+
+    /**
+     * Before lifecycle method for update method.
+     *
+     * @param   string                  $id
+     * @param   \stdClass               $data
+     * @param   EntityInterface|Entity $entity
+     */
+    public function beforeUpdate(string &$id, \stdClass $data, EntityInterface $entity)
+    {
+        $this->processFormData($data);
+    }
+
+    /**
+     * Method to convert workshop GUID to entity reference.
+     *
+     * @param   \stdClass   $data
+     */
+    private function processFormData(\stdClass $data)
+    {
+        if (property_exists($data, 'workshop') &&
+            $data->workshop !== null &&
+            !($data->workshop instanceof WorkshopEntity)
+        ) {
+            $data->workshop = $this->workshopService->getReference(
+                is_object($data->workshop) ? $data->workshop->id : $data->workshop
+            );
+        }
+    }
 }
